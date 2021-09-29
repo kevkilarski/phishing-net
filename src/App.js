690b0c10c72791d402d2d2cca0ea9ec045f59aa8
kevkilarski <kskilarski@gmail.com>
2021-09-28 23:31:00 -0400

    Initialized wireframe structure for JSX and CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div className="wrapper">
+          <h1>Phishing Net</h1>
+        </div>
       </header>
-    </div>
+      <section className="input">
+        <div className="wrapper">
+          <form>
+            <label>
+            <input type="text"></input>
+            </label>
+          </form>
+          <button>Submit URL</button>
+        </div>
+      </section>
+    </>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// --Pseudocode--
+
+// Create landing Page that contains
+// - the header (title)
+// - text form submit button
+
+// Create Components
+// - input (enter url)
+// - output
+//     - clean message
+//     - suspicious message
+
+// Mount Components
+
+// Create State items to hold data for render
+// - user input
+// - PhishStats API
+
+// Add Event for 'submit' button, to pass infomation to userInput component
+
+// Add useEffect for condition for determining if clean or suspicious message will appear
+
+// Add props to Components
+// - cleanMessage
+//     - Condition for appearing or not
+//     - API json properties (Country of Origin, Times Identified as Phishing)
+// - suspiciousMessage
+//     - Condition for appearing or not
+//     - API json properties (Country of Origin, Times Identified as Phishing)
+
+// Render: Upon button press, send API information to useEffect
+// - pass API information to and render clean or suspicious component
+
